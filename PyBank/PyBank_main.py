c5621305cfd9f666564b189b3c38b7668f503dc5
Bowie Hand <bowielh@hotmail.com>
2018-08-04 15:35:26 -0500

    Added comments to the code.

--- PyBank/PyBank_main.py
+++ PyBank/PyBank_main.py
@@ -1,51 +1,62 @@
+#import libraries
 from pathlib import Path
 import csv
 from datetime import datetime as dt
 
+#abspath to resource data
 data_folder_in = Path("/01_Data_Analytics/Git_Clones/UTAUS201807DATA2/\
 homework-instructions/03-Python-Instructions/Instructions/PyBank/Resources/")
 file_to_open = data_folder_in / "budget_data.csv"
 
+#read in data to a list
 data_list = []
 with open(file_to_open, 'r', newline='') as f_o:
     reader = csv.reader(f_o)
-    csv_header = next(f_o)
+    csv_header = next(f_o) #remove header row
     for row in reader:
         data_list.append(row)
 
+#initialize variables
 tot_months = len(data_list)
 tot_income = int(data_list[0][1])
 tot_change = 0
 
+#initialize lists
 g_inc = int(data_list[0][1])
 g_dec = int(data_list[0][1])
 
+#loop through each item in the list
 for i in range(1,len(data_list)):
-        tot_income += int(data_list[i][1])
-        tot_change += int(data_list[i][1]) - int(data_list[i-1][1])
+        tot_income += int(data_list[i][1]) #accrete total income
+        tot_change += int(data_list[i][1]) - int(data_list[i-1][1]) #accrete change in income
 
+        #evaluate for max change in income
         if int(data_list[i][1]) - int(data_list[i-1][1]) > g_inc:
-            g_inc = int(data_list[i][1]) - int(data_list[i-1][1])
-            g_inc_m = dt.strptime(data_list[i][0], '%b-%y').strftime('%b-%Y')
+            g_inc = int(data_list[i][1]) - int(data_list[i-1][1]) #delta between current row and previous
+            g_inc_m = dt.strptime(data_list[i][0], '%b-%y').strftime('%b-%Y') #corresponding month converted to 'Mon-YYYY' format
 
+        #evaluate for min change in income
         elif int(data_list[i][1]) - int(data_list[i-1][1]) < g_dec:
-            g_dec = int(data_list[i][1]) - int(data_list[i-1][1])
-            g_dec_m = dt.strptime(data_list[i][0], '%b-%y').strftime('%b-%Y')
+            g_dec = int(data_list[i][1]) - int(data_list[i-1][1]) #delata between current row and previous
+            g_dec_m = dt.strptime(data_list[i][0], '%b-%y').strftime('%b-%Y') #corresponding month converted to 'Mon-YYYY' format
 
-avg_change = tot_change / (tot_months-1)
+avg_change = tot_change / (tot_months-1) #average change
 
+#store output in a sequence of variables
 line1 = ("Financial Analysis\n")
 line2 = ("---------------------------------------\n")
 line3 = ("Total Months: " + str(tot_months) + "\n")
-line4 = ("Total: " + str('${:,.2f}'.format(tot_income)) + "\n")
-line5 = ("Average Change: " + str('${:,.2f}'.format(avg_change)) + "\n")
+line4 = ("Total: " + str('${:,.2f}'.format(tot_income)) + "\n") #format for currency
+line5 = ("Average Change: " + str('${:,.2f}'.format(avg_change)) + "\n") #format for currency
 line6 = ("Greatest Increase in Profits: " + str(g_inc_m) + " " + \
-str('${:,.2f}'.format(g_inc)) + "\n")
+str('${:,.2f}'.format(g_inc)) + "\n") #format for currency
 line7 = ("Greatest Decrease in Profits: " + str(g_dec_m) + " " + \
-str('${:,.2f}'.format(g_dec)) + "\n")
+str('${:,.2f}'.format(g_dec)) + "\n") #format for currency
 
+#print results to screen
 print(line1, line2, line3, line4, line5, line6, line7, sep='')
 
+#write results to file
 data_folder_out = Path("/01_Data_Analytics/Git_Clones/03_Python/PyBank/")
 file_to_write = data_folder_out / "BankPy.txt"
 f_c = open(file_to_write, 'w')
--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -1,45 +1,54 @@
+#import libraries
 from pathlib import Path
 import csv
 
+#abspath to resource data
 data_folder_in = (Path("/01_Data_Analytics/Git_Clones/UTAUS201807DATA2/\
 homework-instructions/03-Python-Instructions/Instructions/PyPoll/Resources/"))
 file_to_open = data_folder_in / "election_data.csv"
 
+#read in data to a list
 data_list = []
 with open(file_to_open, 'r', newline='') as f_o:
     reader = csv.reader(f_o)
-    csv_header = next(f_o)
+    csv_header = next(f_o) #remove header row
     for row in reader:
         data_list.append(row)
 
+#initialize lists
 cand_list = []
 cand_tot = []
 cand_per = []
 line_list = []
 
+#loop through each item in the list
 for data in data_list:
     if data[2] not in cand_list:
-        cand_list.append(data[2])
-        cand_tot.append(1)
+        cand_list.append(data[2]) #add to candidate list
+        cand_tot.append(1) #add to counter list
     else:
-        cand_tot[cand_list.index(data[2])] += 1
+        cand_tot[cand_list.index(data[2])] += 1 #accrete counter list
 
+#determine total votes and winning candidate
 total_votes = sum(cand_tot)
 winner = cand_list[cand_tot.index(max(cand_tot))]
 
+#create list for output
 line_list.append("Election Results\n")
 line_list.append("-----------------------------\n")
 line_list.append("Total Votes: " + str(total_votes) + "\n")
 line_list.append("-----------------------------" + "\n")
 for i in range(4, len(cand_list)+4):
     line_list.append(cand_list[i-4] + ": " + str(format(cand_tot[i-4]/total_votes,\
-    '.3%')) + " (" + str(cand_tot[i-4]) + ")\n")
+    '.3%')) + " (" + str(cand_tot[i-4]) + ")\n") #formate output for individual candidate totals
 line_list.append("-----------------------------\n")
 line_list.append("Winner: " + winner + "\n")
 line_list.append("-----------------------------\n")
 
+#print output to screen
 print(*line_list, sep = '')
 
+#write output to file
 data_folder_out = Path("/01_Data_Analytics/Git_Clones/03_Python/PyPoll/")
 file_to_write = data_folder_out / "Pollpy.txt"
 f_c = open(file_to_write, 'w')
